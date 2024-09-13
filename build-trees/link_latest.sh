af9bc1ddca6c89787468c9fd69883103dab4c30d
Owen <owen@iris.easytarget.org>
2024-09-13 16:56:16 +0200

    Add choices to link-latest

--- build-trees/link_latest.sh
+++ build-trees/link_latest.sh
@@ -3,22 +3,58 @@
 #
 
 dtc=/usr/bin/dtc
-revision=`/usr/bin/uname -r`
-current=`pwd`
+cdir=`pwd`
+versions=`dpkg --list | grep linux-image-[0-9].* | cut -d" " -s -f 3 | sed s/^linux-image-// | sort -V`
+current=`/usr/bin/uname -r`
+revision=`echo $versions | tail -1`
 out=./latest
 
+echo -e "\nAvailable kernels:"
+option=0
+declare -a klist
+for ver in $versions ; do
+    option=$((option+1))
+    klist[$option]=$ver
+    echo -n "  [$option]  $ver"
+    if [ $ver == $current ] ; then
+        echo " - current running kernel"
+    else
+        echo
+    fi
+done
+
+read -p "Which kernel to link? [$option]: " choice
+if [ -z "$choice" ] ; then choice=$option ; fi
+
+revision=${klist[$choice]}
+if [ -z "$revision" ] ; then
+    echo "No valid kernel selected, exiting."
+    exit
+fi
+
 if [ -d "$revision" ]; then
-    echo "Linking $out to device trees in $revision"
+    echo "Cleaning '$out' and creating new links to device tree binaries in '$revision'"
+    mkdir -p $out
+    rm -f $out/*.dtb
 else
-    echo "No builds for current kernel version: $revision"
-    echo "Try running ./make_trees.sh to generate it."
+    echo "No builds for selected kernel version: $revision"
+    echo "  Try running ./make_trees.sh to generate them"
     exit 1
 fi
 
-mkdir -p $out
-
 for file in `ls $revision/*.dtb`; do
     link=`echo "$file" | sed "s/$revision\/$revision-//g"`
-    echo "  $out/$link --> $current/$file"
-    ln -s "$current/$file" "$out/$link"
+    echo "  $out/$link --> $cdir/$file"
+    ln -sf "$cdir/$file" "$out/$link"
 done
+
+# Test for link in /etc/flash-kernel/dtbs ?
+
+echo
+read -p "Run 'flash-kernel' to apply linked device tree (requires sudo)? [Y]: " choice
+if [[ "$choice" == [Yy]* ]] || [ -z "$choice" ] ; then
+    sudo flash-kernel
+    echo -e "\nIf flash-kernel was successful and configured properly the new device tree will be used after reboot"
+fi
+echo
+# fin
--- build-trees/make_dtb.sh
+++ build-trees/make_dtb.sh
@@ -3,43 +3,51 @@
 #
 
 dtc=/usr/bin/dtc
-revision=`/usr/bin/uname -r`
 # alt=../alt-trees -- disabled auto building of all alt trees, better to do individually
 
-echo "Compiling against headers for $revision"
+cdir=`pwd`
+versions=`dpkg --list | grep linux-image-[0-9].* | cut -d" " -s -f 3 | sed s/^linux-image-// | sort -V`
 
-if [ -d "$revision" ]; then
-    echo "Cleaning and Using existing build directory"
-    rm $revision/*.dts $revision/*.dtsi $revision/*.dtb
-else
-    echo "Creating new build directory: $revision"
-    mkdir "$revision"
-fi
+echo -ne "\nInstalled kernels: "
+echo `echo $versions | sed "s/ /, /g"`
 
-#echo "Copying custom dts sources to build root"
-#for dts in `ls -d $alt/*/*.dts`; do
-#    echo "$dts"
-#    cp $dts .
-#done
+for revision in $versions ; do
+    cd $cdir
+    echo -e "\nCompiling against headers for $revision"
 
-echo "Precompiling all includes in build root into $revision build directory"
-for file in `ls *.dtsi`; do
-    echo "  $file -> $revision/${file##*/}"
-    cpp -I/usr/src/linux-headers-$revision/include/ -nostdinc -undef -x assembler-with-cpp $file > $revision/${file##*/}
+    if [ -d "$revision" ]; then
+        echo "Cleaning and Using existing build directory"
+        rm $revision/*.dts $revision/*.dtsi $revision/*.dtb
+    else
+        echo "Creating new build directory: $revision"
+        mkdir "$revision"
+    fi
+
+    #echo "Copying custom dts sources to build root"
+    #for dts in `ls -d $alt/*/*.dts`; do
+    #    echo "$dts"
+    #    cp $dts .
+    #done
+
+    echo "Precompiling all includes in build root into $revision build directory"
+    for file in `ls *.dtsi`; do
+        echo "  $file -> $revision/${file##*/}"
+        cpp -I/usr/src/linux-headers-$revision/include/ -nostdinc -undef -x assembler-with-cpp $file > $revision/${file##*/}
+    done
+
+    echo "Precompiling all sources in build root into $revision build directory"
+    for file in `ls *.dts`; do
+        echo "  $file -> $revision/${file##*/}"
+        cpp -I/usr/src/linux-headers-$revision/include/ -nostdinc -undef -x assembler-with-cpp $file > $revision/${file##*/}
+    done
+
+    echo "Compiling all device tree sources in $revision build directory"
+    cd $revision
+    for file in `ls *.dts`; do
+        out=${file/.dts/.dtb}
+        echo "  $revision/$file -> $revision/$revision-$out"
+        $dtc $file > $revision-$out
+    done
 done
 
-echo "Precompiling all sources in build root into $revision build directory"
-for file in `ls *.dts`; do
-    echo "  $file -> $revision/${file##*/}"
-    cpp -I/usr/src/linux-headers-$revision/include/ -nostdinc -undef -x assembler-with-cpp $file > $revision/${file##*/}
-done
-
-echo "Compiling all device tree sources in $revision build directory"
-cd $revision
-for file in `ls *.dts`; do
-    out=${file/.dts/.dtb}
-    echo "  $revision/$file -> $revision/$revision-$out"
-    $dtc $file > $revision-$out
-done
-
-echo "Success. Consider running link-latest.sh to make permanent (see docs)"
+echo -e "\nSuccess. Consider running 'link-latest.sh' to make permanent (see docs)"
